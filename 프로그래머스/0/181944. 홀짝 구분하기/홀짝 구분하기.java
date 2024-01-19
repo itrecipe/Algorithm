abbc4bc1ee2683ecbd9124c5487f2b9cc01f222f
ITRecipe <itrecipe@naver.com>
2024-01-19 15:24:38 +0900

    [level 0] Title: 홀짝 구분하기, Time: 186.32 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -5,10 +5,11 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-        if(n % 2 == 0) {
-            System.out.println(n + " " + "is even");
-        } else {
-            System.out.println(n + " " + "is odd");
+        if(n % 2 == 0) { 
+            //n을 2로 나눈 나머지가 0이면 짝수, 아니면 홀수 라고 판별 해줄 조건문, 조건식이 true인 경우 수행한다.
+            System.out.println(n + " " + "is even"); //나머지가 없으면 (0이면) 짝수
+        } else { //조건식이 flase일때 수행되는 문장
+            System.out.println(n + " " + "is odd"); //나머지가 있으면 (1이거나 그 이상이면) 홀수
         }
     }
 }
