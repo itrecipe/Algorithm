4da0e1a53fae5b73ee4c4e326c5be5980671990e
ITRecipe <itrecipe@naver.com>
2024-03-14 20:46:16 +0900

    [level 0] Title: 공배수, Time: 0.04 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/README.md
+++ 프로그래머스/0/181936. 공배수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 공배수 - 181936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181936) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181936?language=java) 
 
 ### 성능 요약
 
-메모리: 76.9 MB, 시간: 0.02 ms
+메모리: 78.2 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 14일 20:23:40
+2024년 03월 14일 20:46:15
 
 ### 문제 설명
 
--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -1,6 +1,19 @@
 class Solution {
     public int solution(int number, int n, int m) {
-     
+
+        //삼항 연산자를 활용한 풀이
         return (number % n == 0 && number % m == 0) ? 1 : 0;
+        
+        //if 조건문을 활용한 풀이
+        /*
+        if(number % n == 0) {
+            System.out.println("if : " + (number % n == 0));
+            return 1;
+            
+        } else if(number % m == 0) {
+            System.out.println("else if : " + (number % m == 0));
+        }
+        return 0;
+        */
     }
 }
